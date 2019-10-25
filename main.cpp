491f64864e2979d9cb05027b5bbb795e8f14ec0f
Piero Angelo <piero.morales@utec.edu.pe>
2019-10-24 19:11:22 -0500

    Base de grafo

--- main.cpp
+++ main.cpp
@@ -1,83 +1,69 @@
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
-// stores adjacency list items
-struct adjNode {
-    int val, cost;
-    adjNode* next;
-};
-// structure to store edges
-struct graphEdge {
-    int start_ver, end_ver, weight;
-};
-class DiaGraph{
-    // insert new nodes into adjacency list from given graph
-    adjNode* getAdjListNode(int value, int weight, adjNode* head) 	{
-        adjNode* newNode = new adjNode;
-        newNode->val = value;
-        newNode->cost = weight;
 
-        newNode->next = head;   // point new node to current head
-        return newNode;
-    }
-    int N;  // number of nodes in the graph
-public:
-    adjNode **head;                //adjacency list as array of pointers
-    // Constructor
-    DiaGraph(graphEdge edges[], int n, int N)  {
-        // allocate new node
-        head = new adjNode*[N]();
-        this->N = N;
-        // initialize head pointer for all vertices
-        for (int i = 0; i < N; ++i)
-            head[i] = nullptr;
-        // construct directed graph by adding edges to it
-        for (unsigned i = 0; i < n; i++)  {
-            int start_ver = edges[i].start_ver;
-            int end_ver = edges[i].end_ver;
-            int weight = edges[i].weight;
-            // insert in the beginning
-            adjNode* newNode = getAdjListNode(end_ver, weight, head[start_ver]);
+struct nodoListaAdyacencia {
+    int data;
+    nodoListaAdyacencia* next;
+};
 
-            // point head pointer to new node
-            head[start_ver] = newNode;
+struct listaAdyacencia {
+    nodoListaAdyacencia *head;
+};
+
+class Graph {
+    private:
+        int vertices;
+        listaAdyacencia* nodosGrafo;
+
+    public:
+        Graph(int vertices) {
+            this->vertices = vertices;
+            nodosGrafo = new listaAdyacencia [vertices];
+            for (int i = 0; i < vertices; ++i)
+                nodosGrafo[i].head = NULL;
         }
-    }
-    // Destructor
-    ~DiaGraph() {
-        for (int i = 0; i < N; i++)
-            delete[] head[i];
-        delete[] head;
-    }
+        
+        nodoListaAdyacencia* newnodoListaAdyacencia(int destino) {
+            nodoListaAdyacencia* nuevoNodo = new nodoListaAdyacencia;
+            nuevoNodo->data = destino;
+            nuevoNodo->next = NULL;
+            return nuevoNodo;
+        }
+        
+        void addEdge(int origen, int destino) {
+            nodoListaAdyacencia* nuevoNodo = newnodoListaAdyacencia(destino);
+            nuevoNodo->next = nodosGrafo[origen].head;
+            nodosGrafo[origen].head = nuevoNodo;
+            // Esta parte determina si es dirigido o no
+            nuevoNodo = newnodoListaAdyacencia(origen);
+            nuevoNodo->next = nodosGrafo[destino].head;
+            nodosGrafo[destino].head = nuevoNodo;
+        }
+        
+        void printGraph() {
+            int v;
+            for (v = 0; v < vertices; ++v) {
+                nodoListaAdyacencia* pCrawl = nodosGrafo[v].head;
+                if (pCrawl) {
+                    cout << "\nAdjacency list of vertex " << v << "\n head ";
+                }
+                while (pCrawl) {
+                    cout<<"-> "<<pCrawl->data;
+                    pCrawl = pCrawl->next;
+                }
+                //if (pCrawl) cout << endl << endl;
+            }
+        }
 };
-// print all adjacent vertices of given vertex
-void display_AdjList(adjNode* ptr, int i)
-{
-    while (ptr != nullptr) {
-        cout << "(" << i << ", " << ptr->val
-             << ", " << ptr->cost << ") ";
-        ptr = ptr->next;
-    }
-    cout << endl;
-}
-// graph implementation
-int main()
-{
-    // graph edges array.
-    graphEdge edges[] = {
-            // (x, y, w) -> edge from x to y with weight w
-            {0,1,2},{0,2,4},{1,4,3},{2,3,2},{3,1,4},{4,3,3}
-    };
-    int N = 6;      // Number of vertices in the graph
-    // calculate number of edges
-    int n = sizeof(edges)/sizeof(edges[0]);
-    // construct graph
-    DiaGraph diagraph(edges, n, N);
-    // print adjacency list representation of graph
-    cout<<"Graph adjacency list "<<endl<<"(start_vertex, end_vertex, weight):"<<endl;
-    for (int i = 0; i < N; i++)
-    {
-        // display adjacent vertices of vertex i
-        display_AdjList(diagraph.head[i], i);
-    }
+
+int main() {
+    Graph gh(5);
+    gh.addEdge(0, 1);
+    gh.addEdge(1, 0);
+ 
+    gh.printGraph();
+ 
     return 0;
 }
