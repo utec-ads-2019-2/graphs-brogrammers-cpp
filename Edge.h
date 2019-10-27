efa68bb27988e072187dd31c29e4e9b98cea8739
Piero Angelo Morales Alcalde <38793411+Piero16301@users.noreply.github.com>
2019-10-27 03:48:57 -0500

    Densidad

--- Edge.h
+++ Edge.h
@@ -11,22 +11,9 @@ public:
     double peso = 0;
     nodoListaAdyacencia* next = nullptr;
 
-    nodoListaAdyacencia(int origen, int destino, std::map <int, Airport*> &data) {
-        double radioTierra = 6371;
-        Airport *airportOrigen = data[origen];
-        Airport *airportDestino = data[destino];
-        double distanciaLatitud = gradosARadianes(airportDestino->latitud - airportOrigen->latitud);
-        double distanciaLongitud = gradosARadianes(airportDestino->longitud - airportOrigen->longitud);
-        double a = sin(distanciaLatitud / 2) * sin(distanciaLatitud / 2) + cos(gradosARadianes(airportOrigen->latitud)) * cos(gradosARadianes(airportDestino->latitud)) * sin(distanciaLongitud / 2) * sin(distanciaLongitud / 2);
-        double c = 2 * atan2(sqrt(a), sqrt(1 - a));
-        double distancia = radioTierra * c;
-        peso = distancia;
-        idDestino = destino;
-    }
+    nodoListaAdyacencia() = default;
 
-    static double gradosARadianes(double grados) {
-        return grados * (M_PI / 180);
-    }
+    nodoListaAdyacencia(int origen, int destino, double peso) : idDestino{destino}, peso{peso} {}
 
 };
 
