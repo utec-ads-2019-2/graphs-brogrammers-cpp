936889a4203053518e7316bc343f42acfa5f9e20
Angel Motta <angelmotta@gmail.com>
2019-10-26 19:28:31 -0500

    Longitude and Latitude

--- Airport.h
+++ Airport.h
@@ -7,6 +7,11 @@
 using namespace std;
 class Airport{
     vector<int> destinations;
+    std::string city;
+    std::string name;
+    std::string country;
+    double longitude;
+    double latitude;
 
     explicit Airport(vector<int> _dest) : destinations{std::move(_dest)} {};
 
